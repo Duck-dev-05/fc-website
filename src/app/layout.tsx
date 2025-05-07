8ac2e6403780dcbd18b81f6ce270b9d14c116752
Duck <minhducworking05@gmail.com>
2025-05-07 21:04:10 +0700

    Update code to deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import { Inter } from 'next/font/google'
 import Providers from '@/components/Providers'
 import ClientLayout from '@/components/ClientLayout'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'FC ESCUELA',
@@ -16,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`min-h-screen bg-gray-50 ${inter.className}`}>
+      <body className="min-h-screen bg-gray-50 font-sans">
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
