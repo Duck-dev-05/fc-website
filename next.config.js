efb11636f6b266a2c29a3c24a705569d50216b89
Duck <minhducworking05@gmail.com>
2025-05-07 20:46:05 +0700

    Reset Database:

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  serverOptions: {
-    hostname: 'localhost',
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 }
 
